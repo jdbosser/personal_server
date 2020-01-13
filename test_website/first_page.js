a6de9dcd4bd8d9f2a1348df3713491bb072e78ea
John Daniel Bossér <daniel@bosser.com>
2020-01-13 09:59:22 +0100

    Adds an email adress to the website. Pray to the god of bots that I wont be spammed. :w

---
+++ test_website/first_page.js
@@ -0,0 +1,11 @@
+// Obscure little bit to avoid spam.
+// I hope that I don't look like a fool doing this 
+
+name = "john"
+otherName = "daniel"
+lastName = "bosser"
+topLevelDomain = ".com"
+specialSymbol = "@"
+
+element = document.getElementsByClassName("secret")[0]
+element.innerHTML = name + "." + otherName + specialSymbol + lastName + topLevelDomain
