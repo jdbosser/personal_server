70282804cf01eb193bda50ca0ecd97ad9d40b120
John Daniel Bosser <john.daniel@bosser.com>
2021-11-30 11:35:41 +0100

    Adds nix flake, testing

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: {
+
+	nixosModule = {
+	
+	{config} : 
+	let 
+	
+	vHost = { root = ./test_website;
+	    forceSSL = true;
+	    enableACME = true;
+	    locations= {
+		"/" = {
+			tryFiles = "$uri $uri/index.html";
+		};
+	    };
+	};
+	in
+	{
+		
+			
+	config.security.acme.email = "john.daniel@bosser.com";
+	config.security.acme.acceptTerms = true;
+
+	# Setup nginx
+	config.services.nginx.enable = true;
+
+	# Setup default virtualhost
+	config.services.nginx.virtualHosts = {
+	      # "192.168.0.24" = vHost;
+	      "bosser.se" = vHost;
+	      "www.bosser.se" = vHost;
+	      # "johndaniel.work" = vHost;
+	      # "www.johndaniel.work" = vHost;
+	      # "johndaniel.engineer" = vHost;
+	      # "www.johndaniel.engineer" = vHost;
+	};
+
+	
+	}
+
+	};
+
+  };
+}
